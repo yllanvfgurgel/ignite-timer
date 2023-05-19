bc2295de9e3e59bc0203a485221f724762cfed16
Yllan Gurgel <the_yllan@hotmail.com>
2023-05-18 22:03:18 -0300

    adicionando config do styled components, eslint no projeto e tema da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/defaultTheme'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-
-  return <h1>Hello World</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
-
